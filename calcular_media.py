d92ff8f601b1af6bab510442ae003c47ab605847
EricLohan <115292109+EricLohan@users.noreply.github.com>
2023-08-22 15:18:46 -0300

    Add files via upload

---
+++ calcular_media.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+@dataclass
+class Aluno:
+    nome: str
+    idade: int
+    notas: list
+
+    def calcular_media(self):
+        return sum(self.notas) / len(self.notas)
+
+aluno1 = Aluno("Raphael", 39, [9.5, 8.7, 7.2])
+aluno2 = Aluno("Caroline", 30, [6.8, 7.5, 8.9])
+
+lista_alunos = [aluno1, aluno2]
+
+for aluno in lista_alunos:
+    print(f"Nome: {aluno.nome}, Média: {aluno.calcular_media():.2f}")
